7d7bc24fb5323c79120944fabf4edd30dcc1ebf1
Dileep329 <spdileep32@gmail.com>
2025-06-04 18:07:48 +0530

    04 JUN 2025 EVENING

--- UserService/src/main/java/com/farmers/friend/UserService/UserServiceApplication.java
+++ UserService/src/main/java/com/farmers/friend/UserService/UserServiceApplication.java
@@ -4,10 +4,8 @@ import io.swagger.v3.oas.annotations.OpenAPIDefinition;
 import io.swagger.v3.oas.annotations.info.Info;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
-@EnableDiscoveryClient
 @OpenAPIDefinition(info = @Info(title = "User Service API", version = "v1"))
 public class UserServiceApplication {
 
